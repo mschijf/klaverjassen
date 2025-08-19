1cc2a13f2fee30b060aee17e14679a0361e7904b
martinschijf <martinschijf@bol.com>
2025-08-19 18:56:12 +0200

    remove hearts

--- src/main/kotlin/com/cards/controller/hearts/model/GameStatusModelHearts.kt
+++
@@ -1,8 +0,0 @@
-package com.cards.controller.hearts.model
-
-import com.cards.controller.basic.model.GameStatusModel
-
-data class GameStatusModelHearts(
-    val generic: GameStatusModel,
-    val goingUp: Boolean,
-)
--- src/main/kotlin/com/cards/controller/hearts/model/ScoreModelHearts.kt
+++
@@ -1,5 +0,0 @@
-package com.cards.controller.hearts.model
-
-data class ScoreModelHearts(val scoreList: List<RoundScoreHearts>)
-
-data class RoundScoreHearts (val south: Int, val west: Int, val east: Int, val north: Int)

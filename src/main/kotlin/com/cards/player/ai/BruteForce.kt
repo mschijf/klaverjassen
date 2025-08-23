2a3871b06ccfbe185fce3b8ee8908a86a1a17947
martinschijf <martinschijf@bol.com>
2025-08-23 22:30:38 +0200

    First tests and iteration on searching all possibilities

--- src/main/kotlin/com/cards/player/ai/BruteForce.kt
+++ src/main/kotlin/com/cards/player/ai/BruteForce.kt
@@ -1,38 +1,117 @@
 package com.cards.player.ai
 
 import com.cards.game.card.Card
-import com.cards.game.card.CardColor
-import com.cards.game.card.CardRank
 import com.cards.game.klaverjassen.GameStatus
 import com.cards.player.Player
+import com.cards.player.PlayerGroup
+import com.cards.tools.CardCombinations
 
 class BruteForce(
-    val playerToMove: GeniusPlayerKlaverjassen,
+    val playerForWhichWeAnalyze: GeniusPlayerKlaverjassen,
     val analyzer: KlaverjassenAnalyzer) {
 
+    private val player1 = playerForWhichWeAnalyze.tableSide.clockwiseNext()
+    private val player2 = player1.clockwiseNext()
+    private val player3 = player2.clockwiseNext()
+
+    private val game = playerForWhichWeAnalyze.game
+    private val fakeGroup = createFakeGroup()
+    private val combiClass = CardCombinations()
+
+
     fun mostValuableCardToPlay(): Card {
-        return Card(CardColor.HEARTS, CardRank.QUEEN)
-    }
-
-    fun tryCard() {
-        playerToMove.getCardsInHand().forEach { card ->
-            val status = playCard(playerToMove, card)
-            val value = tryRestOfGame()
-            takeCardBack(playerToMove, card)
+        val combinations = getCombinations()
+        val cardValueList = combinations.map { combination ->
+            fakeGroup.getPlayer(player1).setCardsInHand(combination.first)
+            fakeGroup.getPlayer(player2).setCardsInHand(combination.second)
+            fakeGroup.getPlayer(player3).setCardsInHand(combination.third)
+            val valuePerCard = tryCard()
+//            println("$combination    -->  %-3s %4d %-3s %4d ".format(valuePerCard[0].card, valuePerCard[0].value, valuePerCard[1].card, valuePerCard[1].value))
+            valuePerCard
         }
+        val totalCardValue = playerForWhichWeAnalyze.getCardsInHand().map{card -> card to cardValueList.flatten().filter { it.card == card }.sumOf { it.value }}
+        return totalCardValue.maxBy { it.second }.first
     }
 
-    fun tryRestOfGame(): Int {
-        return 0
+    private fun tryCard(): List<CardValue> {
+        val result = mutableListOf<CardValue>()
+        playerForWhichWeAnalyze.getCardsInHand().forEach { card ->
+            playCard(playerForWhichWeAnalyze, card)
+            val value = tryRestOfGame()
+            takeCardBack(playerForWhichWeAnalyze, card)
+            result.add(CardValue(card, value))
+        }
+        return result
+    }
+
+    private fun tryRestOfGame(): Int {
+        val sideToMove = game.getSideToMove()
+        if (game.getCurrentRound().isComplete()) {
+            return game.getCurrentRound().getScore().getDeltaForPlayer(tableSide = playerForWhichWeAnalyze.tableSide)
+        }
+
+        val playerToMove = fakeGroup.getPlayer(sideToMove)
+        if (sideToMove == playerForWhichWeAnalyze.tableSide || sideToMove == playerForWhichWeAnalyze.tableSide.opposite()) {
+            var best = Int.MIN_VALUE
+            playerToMove.getLegalPlayableCards().forEach { card ->
+                playCard(playerToMove, card)
+                val v = tryRestOfGame()
+                takeCardBack(playerToMove, card)
+                if (v > best)
+                    best = v
+            }
+            return best
+
+        } else {
+            var best = Int.MAX_VALUE
+            playerToMove.getLegalPlayableCards().forEach { card ->
+                playCard(playerToMove, card)
+                val v = tryRestOfGame()
+                takeCardBack(playerToMove, card)
+                if (v < best)
+                    best = v
+            }
+            return best
+        }
     }
 
     private fun playCard(player: Player, card: Card): GameStatus {
         player.removeCard(card)
-        return player.game.playCard(card)
+        return game.playCard(card)
     }
 
     private fun takeCardBack(player: Player, card: Card) {
-        player.game.takeLastCardBack()
+        game.takeLastCardBack()
         player.addCard(card)
     }
+
+    private fun createFakeGroup(): PlayerGroup {
+
+        return PlayerGroup(
+            listOf(
+                playerForWhichWeAnalyze,
+                Player(player1, game),
+                Player(player2, game),
+                Player(player3, game))
+            )
+    }
+
+    private fun getCombinations(): List<Triple<List<Card>, List<Card>, List<Card>>> {
+        val yy = combiClass.getPossibleCardCombinations(
+            analyzer.cardsInHandForSide(player1),
+            analyzer.cardsInHandForSide(player2),
+            analyzer.cardsInHandForSide(player3),
+
+            analyzer.playerCanHaveCards(player1),
+            analyzer.playerCanHaveCards(player2),
+            analyzer.playerCanHaveCards(player3),
+
+            analyzer.playerSureHasCards(player1),
+            analyzer.playerSureHasCards(player2),
+            analyzer.playerSureHasCards(player3))
+
+        return yy
+    }
 }
+
+data class CardValue(val card: Card, val value: Int)
--- src/test/kotlin/com/cards/player/ai/BruteForceTest.kt
+++ src/test/kotlin/com/cards/player/ai/BruteForceTest.kt
@@ -1,9 +1,9 @@
 package com.cards.player.ai
 
 import com.cards.game.card.Card
+import com.cards.game.card.CardColor
 import com.cards.game.klaverjassen.Game
 import com.cards.game.klaverjassen.TableSide
-import com.cards.player.PlayerGroup
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
@@ -13,35 +13,52 @@ class BruteForceTest {
     @Test
     fun mostValuableCardToPlay() {
         val game = Game()
-        val playerGroup = PlayerGroup(
-            listOf(
-                GeniusPlayerKlaverjassen(TableSide.WEST, game),
-                GeniusPlayerKlaverjassen(TableSide.NORTH, game),
-                GeniusPlayerKlaverjassen(TableSide.EAST, game),
-                GeniusPlayerKlaverjassen(TableSide.SOUTH, game),
-            )
-        )
+        game.startNewRound(CardColor.CLUBS, TableSide.WEST)
+        prepareGame(game)
+
         val analyzer: KlaverjassenAnalyzer = mockk()
-//        val trick = Trick(TableSide.EAST)
         setUp(analyzer)
 
-        println(analyzer.playerCanHaveCards(TableSide.SOUTH))
-        println(analyzer.playerSureHasCards(TableSide.SOUTH))
+        val playerSouth = GeniusPlayerKlaverjassen(TableSide.SOUTH, game)
+        playerSouth.setCardsInHand(Card.ofList("KH AH"))
+
+        val bf = BruteForce(playerSouth, analyzer)
+
+        println( bf.mostValuableCardToPlay())
+
     }
 
     fun setUp(analyzer: KlaverjassenAnalyzer) {
         every { analyzer.playerSureHasCards(TableSide.WEST) } returns emptySet()
         every { analyzer.playerSureHasCards(TableSide.NORTH) } returns emptySet()
         every { analyzer.playerSureHasCards(TableSide.EAST) } returns emptySet()
-        every { analyzer.playerSureHasCards(TableSide.SOUTH) } returns emptySet()
 
         every { analyzer.playerCanHaveCards(TableSide.WEST) } returns
-                Card.ofList("8C 9C 10C JC QC").toSet()
+                Card.ofList("8H 9H 10H JH QH").toSet()
         every { analyzer.playerCanHaveCards(TableSide.NORTH) } returns
-                Card.ofList("8C 9C 10C JC QC").toSet()
+                Card.ofList("8H 9H 10H JH QH").toSet()
         every { analyzer.playerCanHaveCards(TableSide.EAST) } returns
-                Card.ofList("8C 9C 10C JC QC").toSet()
-        every { analyzer.playerCanHaveCards(TableSide.SOUTH) } returns
-                Card.ofList("8C 9C 10C JC QC").toSet()
+                Card.ofList("8H 9H 10H JH QH").toSet()
+
+        every { analyzer.playerCanHaveCards(TableSide.SOUTH) } returns emptySet()
+        every { analyzer.playerSureHasCards(TableSide.SOUTH) } returns
+                Card.ofList("KH AH").toSet()
+
+        every { analyzer.cardsInHandForSide(TableSide.WEST)} returns 2
+        every { analyzer.cardsInHandForSide(TableSide.NORTH)} returns 2
+        every { analyzer.cardsInHandForSide(TableSide.EAST)} returns 1
+        every { analyzer.cardsInHandForSide(TableSide.SOUTH)} returns 2
+    }
+
+    private fun prepareGame(game: Game) {
+        val clubs = Card.ofList("7C 8C 9C 10C JC QC KC AC")
+        val diamonds = Card.ofList("7D 8D 9D 10D JD QD KD AD")
+        val spades = Card.ofList("7S 8S 9S 10S JS QS KS AS")
+        val hearts = Card.ofList("7H")
+
+        clubs.forEach { card -> game.playCard(card)}
+        diamonds.forEach { card -> game.playCard(card)}
+        spades.forEach { card -> game.playCard(card)}
+        hearts.forEach { card -> game.playCard(card)}
     }
 }

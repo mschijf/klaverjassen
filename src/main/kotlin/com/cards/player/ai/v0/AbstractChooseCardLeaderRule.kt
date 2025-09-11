84f3df5b5be8684d6cfbaef422f73a62bf332129
martinschijf <martinschijf@bol.com>
2025-09-11 20:00:21 +0200

    remove unnecessary

--- src/main/kotlin/com/cards/player/ai/v0/AbstractChooseCardLeaderRule.kt
+++
@@ -1,8 +0,0 @@
-package com.cards.player.ai.v0
-
-import com.cards.player.Player
-
-abstract class AbstractChooseCardLeaderRule(player: Player): AbstractChooseCardRule(player) {
-
-}
-
--- src/main/kotlin/com/cards/player/ai/v0/CardValue.kt
+++
@@ -1,5 +0,0 @@
-package com.cards.player.ai.v0
-
-import com.cards.game.card.Card
-
-data class CardValue(val card: Card, val value: Int)
